af5b1355068ab76edc57544551158c01d963bcba
Tram Nguyen Mai Hong <nguyenmaihongtram2004@gmail.com>
2025-10-03 01:02:31 +0700

    add tokenized question dir path

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Directory paths
 ROOT_DIR = pathlib.Path('.')
 QUESTIONS_DIR = ROOT_DIR / 'questions'
-TOKENIZED_QUESTION_DIR = ROOT_DIR / 'tokenized_questions'
+TOKENIZED_QUESTIONS_DIR = ROOT_DIR / 'tokenized_questions'
 VISUAL_FEATURES_DIR = ROOT_DIR / 'visual_features'
 SCENE_GRAPHS_DIR = ROOT_DIR / 'scene_graphs'
 META_INFO_DIR = ROOT_DIR / 'meta_info'
